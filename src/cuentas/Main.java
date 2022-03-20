6ff3749ef12ca1869a0036fa61179a04bde8884e
saruni95 <saramiragaya@gmail.com>
2022-03-20 16:51:05 +0100

    First commit

---
+++ src/cuentas/Main.java
@@ -0,0 +1,36 @@
+package cuentas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        operativa_cuenta();
+
+        /*CCuenta cuenta1; //Otra opción
+        double saldoActual;
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        operativa_cuenta(cuenta1); */
+    }
+
+    public static void operativa_cuenta() { //también vale private, le añado double saldoActual; y en los paréntesis CCuenta cuenta1
+        CCuenta cuenta1;  //Si hubiera hecho operativa_cuenta de la otra manera, metería entre los () también float cantidad
+        double saldoActual;
+        float cantidad;
+
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
